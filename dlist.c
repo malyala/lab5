132e5728d8b090226422ef8bb248d5c60c804983
elizabethmchan2 <elizabethmchan2@gmail.com>
2017-04-05 17:06:55 -0400

    Add files via upload

--- dlist.c
+++ dlist.c
@@ -1,52 +1,95 @@
-/* dlist.h
+/* dlist.c
 
-   Declarations for an abstract sequence type backed by a doubly linked list.
+   Implenetation of a doubly linked list sequence
 */
 
-#ifndef DLIST_H_
-#define DLIST_H_
+#include <stdlib.h>
+
+#include "dlist.h"
+#include "dlist_node.h"
 
 // An abstract type for sequences
-dlist_record* dlist (dlist_node* data, dlist_node* head, dlist_node* tail )
+typedef struct dlist_record* dlist
 {
-  dlist_node* n = malloc(sizeof(dlist_node));
-  n->data = data; 
-  n->head = head;
-  n->tail = tail;
-
-}
+  dlist_node* head;
+  dlist_node* tail
+  int size;
+} dlist_record;
 
 
 // return an empty dlist. Make sure to call dlist_free when you're done
 // with it.
-dlist dlist_new();
+dlist dlist_new()
+  {
+  dlist_record l = malloc(sizeof(dlist_record));
+  l->head = NULL;
+  l->tail = NULL;
+  l->size = 0;
+  dlist_free(l);
+  return l;
+}
 
 // returns the number of elements in the dlist. Runs in constant time.
-int dlist_size(dlist l);
+int dlist_size(dlist* l)
+{
+  return l->size;
+}
 
 // appends a new element to the beginning of the dlist. Runs in constant time.
-void dlist_push(dlist l, int elt);
+void dlist_push(dlist l, int elt)
+{
+  l->head = new_node(elt, l->head, !!!!!!!);
+  l->size++;
+}
 
 // removes and returns the first element in the dlist. Runs in constant time.
 // precondition: there is at least one element in the dlist.
-int dlist_pop(dlist l);
+int dlist_pop(dlist l)
+{
+  llist_node* old_head = l->head;
+  int val = old_head->data;
+  l->head = old_head->next;
+  l->size--;
+
+  free(old_head);
+  return val;
+}
 
 // returns the first element in the dlist, without changing the dlist.
 // Runs in constant time.
 // precondition: there is at least one element in the dlist.
-int dlist_peek(dlist l);
+int dlist_peek(dlist l)
+{
+  return l->head->data;
+}
 
 // appends a new element to the end of the dlist. Runs in constant time.
-void dlist_push_end(dlist l, int elt);
+void dlist_push_end(dlist l, int elt)
+{
+  l->tail = new_node(elt, !!!!!, l->tail);
+  l->size++;
+}
 
 // removes and returns the last element in the dlist. Runs in constant time.
 // precondition: there is at least one element in the dlist.
-int dlist_pop_end(dlist l);
+int dlist_pop_end(dlist l)
+{
+  llist_node* old_head = l->tail;
+  int val = old_head->data;
+  l->tail = old_head->prev;
+  l->size--;
+
+  free(old_head);
+  return val;
+}
 
 // returns the last element in the dlist, without changing the dlist.
 // Runs in constant time.
 // precondition: there is at least one element in the dlist.
-int dlist_peek_end(dlist l);
+int dlist_peek_end(dlist l)
+{
+  return l->tail->data;
+}
 
 // adds a new element into the dlist, after n existing elements.
 // Traverses from whichever side of the dlist is closer.
@@ -56,7 +99,10 @@ void dlist_insert(dlist l, int n, int elt);
 // retrieves the nth element of the dlist.
 // Traverses from whichever side of the dlist is closer.
 // precondition: the list has at least (n+1) elements
-int dlist_get(dlist l, int n);
+int dlist_get(dlist l, int n)
+{
+
+}
 
 // sets the nth element of the dlist to a new value.
 // Traverses from whichever side of the dlist is closer.
@@ -68,9 +114,14 @@ int dlist_set(dlist l, int n, int new_elt);
 // Traverses from whichever side of the dlist is closer.
 // precondition: the list has at least (n+1) elements
 // postcondition: returns the removed element
-int dlist_remove(dlist l, int n);
+int dlist_remove(dlist l, int n)
+{
+
+}
 
 // frees an dlist. Takes O(size(l)) steps.
-void dlist_free(dlist l);
-
-#endif
+void dlist_free(dlist l)
+{
+  free_dlist(l->head);
+  free(l);
+}
